1a05dbabc0ecdb1b035f130fe9ff521d4c3390d3
ADKANK <kumarana998@gmail.com>
2025-02-07 13:15:23 +0530

    Minor changes

--- client/src/auth_config.js
+++ client/src/auth_config.js
@@ -1,4 +1,4 @@
 const authConfig = {
-    "clientId": process.env.REACT_APP_AUTH0_CLIENT_ID,
-    "domain": process.env.REACT_APP_AUTH0_DOMAIN,
+    clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
+    domain: process.env.REACT_APP_AUTH0_DOMAIN,
 }
